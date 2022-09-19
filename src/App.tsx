31f96bc5919e9ed49e1b21f9fb193a3b467d3634
Morgan Simon <morgansimon@microsoft.com>
2022-09-19 10:37:46 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Test
+      </header>
+    </div>
+  );
+}
+
+export default App;
